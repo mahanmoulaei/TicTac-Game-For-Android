c5192963c28d901ed0bca242504e11253e4410e8
Mahan <mahanmoulaei@gmail.com>
2021-12-01 23:09:56 -0500

    Finish Front-End

--- app/src/main/java/com/example/tictacgame/MainActivity.java
+++ app/src/main/java/com/example/tictacgame/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.tictacgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int grid_size;
+    TableLayout gameBoard;
+    TextView textViewTurn;
+    char[][] my_board;
+    char turn;
+
+    Button buttonReset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void ResetBoard() {
     }
 }
